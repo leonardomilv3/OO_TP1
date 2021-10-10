348e3b7ba14cce955ec68c0bc77c5019257ef35a
MattSilverio <matheusphillipo@gmail.com>
2021-10-10 03:35:13 -0300

    Classe ListaDeMoradores
    Falta elaborar o método calcularPesos. Os outros estão funcionando

---
+++ ListaDeMoradores.java
@@ -0,0 +1,38 @@
+import java.util.Objects;
+
+public class ListaDeMoradores {
+    private Morador [] listaMoradores;
+
+    public float calcularPesos(){
+
+        return 25.25f;
+    }
+
+    public double calcularPorcentagemIgualitaria(){
+        double porcentagemPagar =  ((100/this.listaMoradores.length) * 0.01 );
+
+        return porcentagemPagar * 100;
+    }
+
+    public String pesquisarMorador(String morador){
+        for (Morador listaMoradore : this.listaMoradores) {
+            if (Objects.equals(morador, listaMoradore.getNome())) {
+                return listaMoradore.getNome() + " mora aqui";
+            }
+        }
+        return "Não Existe esse morador";
+    }
+
+    public String[] mostrarMoradores(int index) {
+        String [] statusMoradores = new String[index];
+        for (int i = 0; i < listaMoradores.length; i++){
+            statusMoradores[i] = listaMoradores[i].getNome() + " " + listaMoradores[i].getEmail() + " " + listaMoradores[i].getTotalDeRendimento();
+        }
+
+        return statusMoradores;
+    }
+
+    public void setListaMoradores(Morador[] listaMoradores) {
+        this.listaMoradores = listaMoradores;
+    }
+}
