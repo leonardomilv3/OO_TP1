26c656e68db89b0a7ac269342b18b901ecc82c29
Renan Lacerda <88076318+LacerdaRenan@users.noreply.github.com>
2021-10-11 08:19:07 -0300

    Update SubcategoriaDespesa.java

--- SubcategoriaDespesa.java
+++ SubcategoriaDespesa.java
@@ -1,3 +1,9 @@
 public class SubcategoriaDespesa{
   private String Subcategoria;
+  private int id;
+  
+  public SubcategoriaDespesa(String Subcategoria, int id){
+    this.Subcategoria = Subcategoria;
+    this.id = id;
+  }
 }
