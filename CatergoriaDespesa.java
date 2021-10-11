55569ac5ec78e32b97501d14d62377051441cb0d
LacerdaRenan <88076318+LacerdaRenan@users.noreply.github.com>
2021-10-10 21:37:48 -0300

    Update CatergoriaDespesa.java

--- CatergoriaDespesa.java
+++ CatergoriaDespesa.java
@@ -1,4 +1,4 @@
 public class CategoriaDespesa {
-    ListaSubcategoriaDespesas[] SubcategoriaDespesas;
+    ListaSubcategoriaDespesas[] SubcategoriaDespesas = new SubcatergoriaDespesas[0];
     private String Categoria;
 }
