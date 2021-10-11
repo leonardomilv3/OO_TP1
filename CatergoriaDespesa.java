88909e4f764ff9d07bf3b5d16230028d3663aee0
LacerdaRenan <88076318+LacerdaRenan@users.noreply.github.com>
2021-10-10 22:35:05 -0300

    Update CatergoriaDespesa.java

--- CatergoriaDespesa.java
+++ CatergoriaDespesa.java
@@ -1,9 +1,12 @@
 public class CategoriaDespesa {
     static SubcategoriaDespesas[] ListaSubcategoriaDespesas = new SubcatergoriaDespesas[0];
     private String Categoria;
+    private int id;
     
-    public CategoriaDespesa(String Categoria){
+    public CategoriaDespesa(String Categoria, int id){
         this.Categoria;
+        this.id;
     }
     //getters
+    //Manipulação de vetores
 }
