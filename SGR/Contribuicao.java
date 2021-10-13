eb8c37c9fe8439f2235d1bdddd0c23cf8b0748d8
Vinícius Alves <viniciusalves999@gmail.com>
2021-10-13 11:38:05 -0300

    Update 1.2
    Criei as classes: CadastroDespesas, Contribuicao, e ArquivoTxt.
    Criei o método main , e importei as classes CadastroMoradores e CadastroDespesas na classe UI.

---
+++ SGR/Contribuicao.java
@@ -0,0 +1,22 @@
+package SGR;
+
+public class Contribuicao {
+	
+	public String calcularContribuicao() {
+		String string = null;
+		return string;
+	}
+	
+	public String igualitaria() {
+		String string = null;
+		return string;
+	}
+	
+	public String proporcional() {
+		String string = null;
+		return string;
+	}
+	
+	
+
+}
--- app/UI.java
+++ app/UI.java
@@ -1,6 +1,8 @@
 package app;
 
 import SGR.CadastroMoradores;
+import SGR.CadastroDespesas;
+
 public class UI {
     public static void main(String[] args) {
 
