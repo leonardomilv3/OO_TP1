664e967144086315de5428aa83e02caeb13b2f72
Vinícius Alves <viniciusalves999@gmail.com>
2021-10-23 20:17:23 -0300

    Desenvolvimento da classe CadastroMoradores.

--- SGR/CadastroMoradores.java
+++ SGR/CadastroMoradores.java
@@ -1,29 +1,56 @@
 package SGR;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
 public class CadastroMoradores implements Impressora {
 
-    static Morador[] moradores = new Morador[0];
+	private List<Morador> moradores;
 
-    public void cadastrarMoradores() {
-    }
+	public CadastroMoradores() {
+		// Usando o construtor para instanciar a lista encadeada
+		// Foi a solucao que encontrei momentanemente
+		moradores = new LinkedList<Morador>();
+	}
 
-    public Morador[] getMoradores() {
-        return moradores;
-    }
+	public void cadastrarMoradores() {
 
-    private float rendimentoTotal() {
-        float rendimentoTotal = 0;
-        return rendimentoTotal;
-    }
+		String nome = JOptionPane.showInputDialog("Nome: ");
+		String email = JOptionPane.showInputDialog("\n" + "Email: ");
+		String strRendimento = JOptionPane.showInputDialog("\n" + "Rendimento: ");
+		float rendimento = Float.parseFloat(strRendimento);
 
-    public float getRendimentoTota() {
-        return rendimentoTotal();
-    }
+		// Instanciando Morador
+		Morador morador = new Morador(nome, email, rendimento);
 
-    @Override
-    public boolean escreveTxt(String caminho) {
-        boolean escreveu = true;
-        return escreveu;
-    }
+		moradores.add(morador); // adicionado o novo morador
+	}
+
+	public List<Morador> getMoradores() {
+		return moradores;
+	}
+
+	private float rendimentoTotal() {
+		
+		float rendimentoTotal = 0;
+
+		for (Morador m : moradores) {
+			rendimentoTotal += m.getRendimento();
+		}
+		return rendimentoTotal;
+	}
+
+	public float getRendimentoTota() {
+		return rendimentoTotal();
+	}
+
+	@Override
+	public boolean escreveTxt(String caminho) {
+		boolean escreveu = true;
+		return escreveu;
+	}
 
 }
--- app/UI.java
+++ app/UI.java
@@ -3,8 +3,42 @@ package app;
 import SGR.CadastroMoradores;
 import SGR.CadastroDespesas;
 
-public class UI {
-    public static void main(String[] args) {
+import javax.swing.JOptionPane;
 
-    }
+public class UI {
+
+	static CadastroMoradores cadastroMoradores = new CadastroMoradores();
+
+	public static void main(String[] args) {
+
+		int opcao;
+
+		do {
+			String menu = "Informe a opcao desejada: \n" 
+						+ "1 - Cadastrar morador \n" 
+						+ "2 - Cadastrar despesa \n"
+						+ "0 - Sair do programa";
+
+			String strOpcao = JOptionPane.showInputDialog(menu);
+			opcao = Integer.parseInt(strOpcao);
+
+			switch (opcao) {
+			case 1:
+				cadastroMoradores.cadastrarMoradores();
+				break;
+
+			case 2:
+				break;
+
+			case 0:
+				break;
+			}
+
+		} while (opcao != 0);
+
+		// apenas um teste
+		JOptionPane.showMessageDialog(null, "Rendimento total: " + cadastroMoradores.getRendimentoTota());
+
+	}
+
 }
