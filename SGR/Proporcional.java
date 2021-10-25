eca4ef5d1344aa13b608b72c1254b52c707e3ee4
Matheus Phillipo <matheusphillipo@gmail.com>
2021-10-24 21:48:56 -0300

    Update Proporcional.java

--- SGR/Proporcional.java
+++ SGR/Proporcional.java
@@ -3,11 +3,16 @@ package SGR;
 public class Proporcional extends Contribuicao{
 
     @Override
-    public String calcularContribuicao(float valorTotal, int numeroMoradores){
+    public String calcularContribuicao(CadastroMoradores moradores, CadastroDespesas despesas){
+        float k = moradores.getRendimentoTotal() / despesas.getValorTotal(); // constante de proporcionalidade
+        String resultado = "";
 
-        return "teste";
+        for(int i = 0 ; i < moradores.getNumeroMoradores(); i++){
+            //resultado += moradores.     Quero fazer um loop em que va adicionando "<nome_morador> pagará X (que será k * rendimento desse morador)
+        }
+
+        return resultado;
     }
 }
-
 
 
