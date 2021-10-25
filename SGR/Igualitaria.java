85f45acaf09208cdaa15b16f08995e1b6652c833
Matheus Phillipo <matheusphillipo@gmail.com>
2021-10-24 21:25:20 -0300

    Atualizando classe Igualitaria

--- SGR/Igualitaria.java
+++ SGR/Igualitaria.java
@@ -3,9 +3,9 @@ package SGR;
 public class Igualitaria extends Contribuicao{
 
     @Override
-    public String calcularContribuicao(float valorTotalDespesa, int numeroMoradores){ // os argumentos devem passar os metodos que retornem os valores desejados
-        float valTotal = valorTotalDespesa;
-        int numMoradores = numeroMoradores;
+    public String calcularContribuicao(CadastroMoradores moradores, CadastroDespesas despesas){ 
+        float valTotal = despesas.getValorTotal();
+        int numMoradores = moradores.getNumeroMoradores();
 
         return "Cada um paga " + 100/numMoradores + "%" + " de " + valTotal;
     }
