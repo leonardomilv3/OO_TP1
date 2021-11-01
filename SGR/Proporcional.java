47b0b064e6377709fa33d3f666a2f48e7b43cf28
Matheus Phillipo <matheusphillipo@gmail.com>
2021-10-31 22:58:09 -0300

    Resolvendo problemas de arredondamento

--- SGR/Proporcional.java
+++ SGR/Proporcional.java
@@ -1,22 +1,17 @@
 package SGR;
 
 public class Proporcional extends Contribuicao{
-    static CadastroMoradores cadastroMoradores = new CadastroMoradores();
-    static CadastroDespesas cadastroDespesas = new CadastroDespesas();
 
     @Override
     public String calcularContribuicao(CadastroMoradores moradores, CadastroDespesas despesas){
-        float k = moradores.getRendimentoTotal() / despesas.getValorTotal();
+        float k =  despesas.getValorTotal() / moradores.getRendimentoTotal();
         String resultado = "";
-
-
 
         for(int i = 0 ; i < moradores.getNumeroMoradores(); i++){
             Morador morador = moradores.getMoradores().get(i);
-            resultado += morador.getNome() + " pagará " + "R$ " + k * morador.getRendimento() + "\n";
+            resultado += morador.getNome() + " contribui " + "R$ " +  arredondar(k * morador.getRendimento()) + ", sendo  " + arredondar((morador.getRendimento()/moradores.getRendimentoTotal()) * 100) + "%" + " de sua renda" + "\n";
         }
 
         return resultado;
     }
-
 }
