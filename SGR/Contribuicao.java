f45e1d7bbe24f98e812f1eaaf0a5826bd81097ef
Matheus Phillipo <matheusphillipo@gmail.com>
2021-10-31 22:54:43 -0300

    Adicionando um novo metodo para arredondamento

--- SGR/Contribuicao.java
+++ SGR/Contribuicao.java
@@ -1,9 +1,16 @@
 package SGR;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public abstract class Contribuicao {
 	
 
 	public abstract String calcularContribuicao(CadastroMoradores moradores, CadastroDespesas despesas);
 
+	protected static String arredondar(double numero) {
+		DecimalFormat df = new DecimalFormat("0.00");
+		df.setRoundingMode(RoundingMode.HALF_UP);
+		return df.format(numero);
+	}
 }
-
